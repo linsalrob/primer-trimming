5a76f3f370717daba2b9b9526fd335562087b5ac
linsalrob <raedwards@gmail.com>
2023-05-13 19:53:44 +0930

    fast reverse complementing integers

---
+++ src/reverse-complement.c
@@ -0,0 +1,29 @@
+#include "reverse-complement.h"
+#include <stdint.h>
+
+uint64_t reverse_complement(uint64_t enc, int len) {
+	/*
+	 * Given a primer encoding and the length of the primer, 
+	 * calculate its reverse complement. Note that we need
+	 * the length otherwise all the leading 00's become 11's
+	 */
+
+        uint64_t r = 0;
+        for (int i=0; i<len; i++) {
+                //left shift, first time, its all 0's
+		r<<=2;
+                // get the value of the two lsbs
+                int l = (enc & 1);
+                enc >>= 1;
+                int h = (enc & 1);
+                enc >>= 1;
+                // are low (l) and high (h) set, 
+		// if l and h: (11) T -> need A (0)
+                // if l not h: (01) C -> need G (2)
+                // if h not l: (10) G -> need C (1)
+                // otherwise need (00) T (3)
+                int t = l & h ? 0 : l ? 2 : h ? 1 : 3;
+                r += t;
+        }
+        return r;
+}
---
+++ src/reverse-complement.h
@@ -0,0 +1,10 @@
+
+#ifndef REVERSE_COMPLEMENT
+#define REVERSE_COMPLEMENT
+
+#include <stdint.h>
+
+uint64_t reverse_complement(uint64_t, int);
+
+#endif
+
