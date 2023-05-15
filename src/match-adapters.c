8314a69308ec1bd81c1736d7f1ecad4051cf3aef
linsalrob <raedwards@gmail.com>
2023-05-15 20:41:02 +0930

    fixing an offset issue with reverse complement primers. Closes #4

--- src/match-adapters.c
+++ src/match-adapters.c
@@ -17,7 +17,8 @@
 #include "version.h"
 #include "compare-seqs.h"
 #include "print-sequences.h"
-#include "reverse-complement.h"
+#include "rob_dna.h"
+#include "seqs_to_ints.h"
 #include "colours.h"
 
 KSEQ_INIT(gzFile, gzread);
@@ -29,6 +30,11 @@ void encode_primers(char* primerfile, kmer_bst_t* primers, int kmer, bool revers
 	 * shorter than this, so if they are we put a warning out
 	 *
 	 * If bool is true, we will include the reverse complement of the primers
+	 *
+	 * Note: we need to take the correct substring of the sequence to reverse complement! We need the rightmose k-bases 
+	 * otherwise we have an offset of length(string) - kmer to where the match should be.
+	 *
+	 *
 	 */
 	if( access( primerfile, R_OK ) == -1 ) {
 		// file doesn't exist
@@ -50,7 +56,8 @@ void encode_primers(char* primerfile, kmer_bst_t* primers, int kmer, bool revers
 		uint64_t enc = kmer_encoding(seq->seq.s, 0, kmer);
 		add_primer(enc, seq->name.s, primers);
 		if (reverse) {
-			enc = reverse_complement(enc, kmer);
+			uint64_t rcenc = kmer_encoding(seq->seq.s, strlen(seq->seq.s) - kmer, kmer);
+			enc = reverse_complement(rcenc, kmer);
 			char revname[strlen(seq->name.s)+3];
 			strcpy(revname, seq->name.s);
 			strcat(revname, " rc");
@@ -91,14 +98,12 @@ void search_seqfile_for_primers(char* seqfile, kmer_bst_t* primers, int kmer, in
 		if (ks) 
 			printf("%s\t%s0\n", ks->id, seq->name.s);
 		
-			// printf("Found primer %s (val: %ld) in sequence %s at position 0\n", ks->id, ks->value, seq->name.s);
-		for (int i=1; i<seq->seq.l - kmer; i++) {
+		for (int i=1; i<seq->seq.l - kmer + 1; i++) {
 			enc = next_kmer_encoding(seq->seq.s, i, kmer, enc);
 			kmer_bst_t *ks = find_primer(enc, primers);
 			if (ks) 
-				printf("%s\t%s\t%d\n", ks->id, seq->name.s, i);
+				printf("%s\t%s\t%d\t%s\n", ks->id, seq->name.s, i, kmer_decoding(enc, kmer));
 		
-			// printf("Found primer %s in sequence %s at position %d\n", ks->id, seq->name.s, i);
 		}
 	}
 }
@@ -108,6 +113,7 @@ void trim_seq_at_primers(char* seqfile, kmer_bst_t* primers, int kmer, char* out
 	/*
 	 * look through the sequences and trim at the start of the primers
 	 * The same as above, but we set seq->seq.s and seq->qual.s to '\0' and write them to a file
+	 *
 	 */
 
 	if( access( seqfile, R_OK ) == -1 ) {
@@ -135,11 +141,13 @@ void trim_seq_at_primers(char* seqfile, kmer_bst_t* primers, int kmer, char* out
 	while ((l = kseq_read(seq)) >= 0) {
 		uint64_t enc = kmer_encoding(seq->seq.s, 0, kmer);
 		kmer_bst_t *ks = find_primer(enc, primers);
+		bool printed = false;
 		if (ks) {
 			fprintf(stderr, "%sIn sequence%s we found primer %s right at the beginning!!%s\n", YELLOW, seq->name.s, ks->id, ENDC);
 			fprintf(stderr, "%s AT the moment, we didn't write it, so you fastq files are no longer paired!%s\n", RED, ENDC);
+			printed = true;
 		} else {
-			for (int i=1; i<seq->seq.l - kmer; i++) {
+			for (int i=1; i<seq->seq.l - kmer + 1; i++) {
 				enc = next_kmer_encoding(seq->seq.s, i, kmer, enc);
 				kmer_bst_t *ks = find_primer(enc, primers);
 				if (ks) {
@@ -157,11 +165,24 @@ void trim_seq_at_primers(char* seqfile, kmer_bst_t* primers, int kmer, char* out
 					gzwrite(writer, "\n+\n", 3);
 					gzwrite(writer, newqua, i);
 					gzwrite(writer, "\n", 1);
+					printed = true;
 					break;
 				}
 
 			}
 		}
+		if (!printed) {
+					gzwrite(writer, "@", 1);
+					gzwrite(writer, seq->name.s, strlen(seq->name.s));
+					gzwrite(writer, " ", 1);
+					gzwrite(writer, seq->comment.s, strlen(seq->comment.s));
+					gzwrite(writer, "\n", 1);
+					gzwrite(writer, seq->seq.s, strlen(seq->seq.s));
+					gzwrite(writer, "\n+\n", 3);
+					gzwrite(writer, seq->qual.s, strlen(seq->qual.s));
+					gzwrite(writer, "\n", 1);
+		}
+
 	}
 	gzclose(writer);
 
