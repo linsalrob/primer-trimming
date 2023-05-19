08a9d8f8909b8b0d3002ba6bd319ca34c11407de
linsalrob <raedwards@gmail.com>
2023-05-20 09:10:38 +0930

    Searching R1 and R2 together for all matches

---
+++ src/find-adapters-pairwise.c
@@ -0,0 +1,9 @@
+/*
+ * Get both an R1 and an R2 file and then trim for the adapters.
+ *
+ * If we find I7-left on R1 we should find rc(I5-right) on R2.
+ *
+ * In this instance, we _ONLY_ search for those primers
+ */
+
+
