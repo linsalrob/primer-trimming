d6477fc09006a3f75ac96e42a84ebc83a723be44
linsalrob <raedwards@gmail.com>
2023-05-20 20:12:18 +0930

    initializing an empty struct

--- src/store-primers.c
+++ src/store-primers.c
@@ -23,6 +23,14 @@ void add_primer(uint64_t encoding, char* primerid, kmer_bst_t* ks) {
 		strcpy(ks->id, primerid);
 		ks->bigger = (kmer_bst_t *) malloc(sizeof(*ks->bigger));
 		ks->smaller = (kmer_bst_t *) malloc(sizeof(*ks->bigger));
+		ks->bigger->bigger = NULL;
+		ks->bigger->smaller = NULL;
+		ks->smaller->bigger = NULL;
+		ks->smaller->smaller= NULL;
+		ks->bigger->value = 0;
+		ks->smaller->value = 0;
+		ks->bigger->id = "";
+		ks->smaller->id = "";
 		return;
 	}
 	if (encoding > ks->value)
